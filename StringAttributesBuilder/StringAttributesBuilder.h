750f350c3b4c4e6e838367dc4f607ddf2cd7f281
Andrey Arzhannikov <dev.andreyarzhannikov@gmail.com>
2020-03-20 00:27:30 +0600

    Initial Commit

---
+++ StringAttributesBuilder/StringAttributesBuilder.h
@@ -0,0 +1,19 @@
+//
+//  StringAttributesBuilder.h
+//  StringAttributesBuilder
+//
+//  Created by Andrey Arzhannikov on 20.03.2020.
+//  Copyright © 2020 Arzhannikov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StringAttributesBuilder.
+FOUNDATION_EXPORT double StringAttributesBuilderVersionNumber;
+
+//! Project version string for StringAttributesBuilder.
+FOUNDATION_EXPORT const unsigned char StringAttributesBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StringAttributesBuilder/PublicHeader.h>
+
+
