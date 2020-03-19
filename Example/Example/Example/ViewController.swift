0091b880a50ba563bdfdb96a059ecfea97fcbd6b
Andrey Arzhannikov <dev.andreyarzhannikov@gmail.com>
2020-03-20 01:31:18 +0600

    Add Example project

---
+++ Example/Example/Example/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Andrey Arzhannikov on 20.03.2020.
+//  Copyright © 2020 Arzhannikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let label: UILabel = .init()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
